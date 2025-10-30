683048c43d0cebd23510c11e4e12fd0c86bf3973
charles-masse <charlesmasseqc@gmail.com>
2025-10-29 23:35:45 -0400

    One path per agent

--- modules/Crowd.js
+++ modules/Crowd.js
@@ -16,66 +16,68 @@ export class Crowd {
         this.clock = new THREE.Clock();
 
         this.entityManager = new YUKA.EntityManager();
-        // Path
-        this.path = new YUKA.Path();
-        this.path.loop = true;
-        this.path.add(new YUKA.Vector3(-2, -0.75, 2));
-        this.path.add(new YUKA.Vector3(-3, -0.75, 0));
-        this.path.add(new YUKA.Vector3(-2, -0.75, -2));
-        this.path.add(new YUKA.Vector3(0, -0.75, 0));
-        this.path.add(new YUKA.Vector3(2, -0.75, -2));
-        this.path.add(new YUKA.Vector3(3, -0.75, 0));
-        this.path.add(new YUKA.Vector3(2, -0.75, 2));
-        this.path.add(new YUKA.Vector3(0, -0.75, 3));
         // Character test
         this.vehicleGeometry = new THREE.ConeGeometry(0.1, 0.5, 8);
         this.vehicleGeometry.rotateX(Math.PI * 0.5);
         this.vehicleMaterial = new THREE.MeshBasicMaterial({color: 0x000000});
 
-        if (debug) {
-
-            const position = [];
-
-            for (let i = 0; i < this.path._waypoints.length; i ++) {
-
-                const waypoint = this.path._waypoints[i];
-
-                position.push(waypoint.x, waypoint.y, waypoint.z);
-
-            }
-
-            const lineGeometry = new THREE.BufferGeometry();
-            lineGeometry.setAttribute('position', new THREE.Float32BufferAttribute(position, 3));
-            const lineMaterial = new THREE.LineBasicMaterial({color: 0xff0000});
-            const lines = new THREE.LineLoop(lineGeometry, lineMaterial);
-            this.scene.add(lines);
-
-        }
     }
 
     spawn() {
+
+        // Path
+        const path = new YUKA.Path();
+        path.loop = true;
+        path.add(new YUKA.Vector3(-2, -0.75, 2));
+        path.add(new YUKA.Vector3(-3, -0.75, 0));
+        path.add(new YUKA.Vector3(-2, -0.75, -2));
+        path.add(new YUKA.Vector3(0, -0.75, 0));
+        path.add(new YUKA.Vector3(2, -0.75, -2));
+        path.add(new YUKA.Vector3(3, -0.75, 0));
+        path.add(new YUKA.Vector3(2, -0.75, 2));
+        path.add(new YUKA.Vector3(0, -0.75, 3));
+
+        // if (debug) {
+
+        //     const position = [];
+
+        //     for (let i = 0; i < path._waypoints.length; i ++) {
+
+        //         const waypoint = path._waypoints[i];
+        //         position.push(waypoint.x, waypoint.y, waypoint.z);
+
+        //     }
+
+        //     const lineGeometry = new THREE.BufferGeometry();
+        //     lineGeometry.setAttribute('position', new THREE.Float32BufferAttribute(position, 3));
+        //     const lineMaterial = new THREE.LineBasicMaterial({color: 0xff0000});
+        //     const lines = new THREE.LineLoop(lineGeometry, lineMaterial);
+        //     this.scene.add(lines);
+
+        // }
 
         const vehicleMesh = new THREE.Mesh(this.vehicleGeometry, this.vehicleMaterial);
         vehicleMesh.matrixAutoUpdate = false;
         this.scene.add(vehicleMesh);
 
         const vehicle = new YUKA.Vehicle();
+        vehicle.maxSpeed = Math.random() + 1;
         vehicle.setRenderComponent(vehicleMesh, sync);
-        // use "FollowPathBehavior" for basic path following
-        const followPathBehavior = new YUKA.FollowPathBehavior(this.path, 0.5);
+
+        const followPathBehavior = new YUKA.FollowPathBehavior(path);
         vehicle.steering.add(followPathBehavior);
-        // use "OnPathBehavior" to realize a more strict path following.
-        const onPathBehavior = new YUKA.OnPathBehavior(this.path);
-        vehicle.steering.add(onPathBehavior);
+
+        // const onPathBehavior = new YUKA.OnPathBehavior(path);
+        // vehicle.steering.add(onPathBehavior);
 
         this.entityManager.add(vehicle);
-        vehicle.position.copy(this.path._waypoints[0]);
+        vehicle.position.copy(path._waypoints[0]);
 
     }
 
     update() {
 
-        if (this.clock.getElapsedTime() > Math.random() * (10 - 3) + 3) {
+        if (this.clock.getElapsedTime() > Math.round(Math.random() * (10 - 3) + 3)) {
 
             this.spawn();
             this.clock.start();
