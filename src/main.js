6bcf29fae9dd019598de2497f1143dd3144240cc
charles-masse <charlesmasseqc@gmail.com>
2026-04-15 00:16:31 -0400

    Generating navmesh from polys

--- src/main.js
+++ src/main.js
@@ -5,41 +5,33 @@ import * as YUKA from 'yuka';
 
 import {createConvexRegionHelper,} from './NavMeshHelper.js';
 import {createGraphHelper,} from './GraphHelper.js';
-
-async function loadNavMesh(path, loadingManager=null) {
-
-    const navMesh = new Promise((resolve) => {
-        
-        new YUKA.NavMeshLoader(loadingManager).then((gltf) => {
-            resolve(gltf);
-        });
-
-    });
-
-    return navMesh;
-}
 //Scene
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(2, 2, 2);
+camera.position.set(0, 5, 0);
 camera.lookAt(0, 0, 0);
 
-// const geometry = new THREE.BoxGeometry(0.1, 0.1, 0.1);
-// const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
-// const cube = new THREE.Mesh(geometry, material);
-// scene.add(cube);
+const poly1 = new YUKA.Polygon().fromContour([
+    new YUKA.Vector3(0,0,2),
+    new YUKA.Vector3(2,0,0),
+    new YUKA.Vector3(0,0,0),
+]);
 
-// const navMesh = new YUKA.NavMesh().fromPolygons([
-//     new YUKA.Polygon().fromContour([new YUKA.Vector3(1, 0, 1), new YUKA.Vector3(-1, 0, 1), new YUKA.Vector3(0, 0, -1)])
-// ]);
+const poly2 = new YUKA.Polygon().fromContour([
+    new YUKA.Vector3(0,0,2),
+    new YUKA.Vector3(2,0,2),
+    new YUKA.Vector3(2,0,0),
+]);
 
-const navMesh = new YUKA.NavMeshLoader().load('navmesh.glb', {'mergeConvexRegions': false}).then((navMesh) => {
-    console.log(navMesh);
-    scene.add(createConvexRegionHelper(navMesh));
-    scene.add(createGraphHelper(navMesh.graph, 0.25));
-});
+const navMesh = new YUKA.NavMesh();
+navMesh.mergeConvexRegions = false;
+navMesh.fromPolygons([poly1, poly2]);
 
+console.log(navMesh);
+
+scene.add(createConvexRegionHelper(navMesh));
+scene.add(createGraphHelper(navMesh.graph, 0.1));
 //Renderer
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
